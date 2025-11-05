49fdf2a87929d1f47bf778c57344474fc0b63996
Coder AK <alexcoder@gmail.com>
2025-11-05 22:10:19 +0200

    add link to tg bot

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,31 +1,20 @@
-import { useLocation } from 'preact-iso';
 import './Header.less';
 import { State } from '../../state/svtilo-app-state';
 
 export function Header() {
-  const { url } = useLocation();
-
   const { data } = State.value;
 
   const light = data.light == null ? 'on' : data.light ? 'on' : 'off';
 
   return (
-    // <header>
-    // 	<nav>
-    // 		<a href="/" class={url == '/' && 'active'}>
-    // 			Home
-    // 		</a>
-    // 		<a href="/404" class={url == '/404' && 'active'}>
-    // 			404
-    // 		</a>
-    // 	</nav>
-    // </header>
-
     <nav className="navbar">
+      <div></div>
       <a className="navbar-brand" href="/">
         <img className="logo-lamp" src={`assets/lamp_${light}.svg`} />
         <img className="logo-image" src="assets/svitlo-logo.svg" alt="Світло є, чи нема?" />
-        <div className="logo-lamp"></div>
+      </a>
+      <a className="navbar-link" href="https://t.me/SvitloeRadujnyBot" target="_blank" title="Svitlo-E телеграм бот">
+        <img className="logo-tg" src={`assets/tg-logo.svg`} alt="Telegram logo" />
       </a>
     </nav>
   );
